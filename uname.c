d86596588c2497f5569c44029fd57132b5c6d6c6
Glavin Wiechert <glavin.wiechert@gmail.com>
2014-09-30 16:43:16 -0300

    Output is the same as desired.

--- uname.c
+++ uname.c
@@ -2,10 +2,11 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <string.h>
+#include <sys/utsname.h>
 
 int main(int argc, char *argv[]) {
 
-        struct UNAME_STRUCT ubuffer;
+    struct utsname ubuffer;
 
     // Check for proper usage
     if (argc != 1) {
@@ -16,6 +17,19 @@ int main(int argc, char *argv[]) {
     uname(&ubuffer);
 
     printf("System name: %s\n", ubuffer.sysname);
+    printf("Host name: %s\n", ubuffer.nodename);
+    printf("OS Release: %s\n", ubuffer.release);
+    printf("OS Version: %s\n", ubuffer.version);
+    printf("CPU Type: %s\n", ubuffer.machine);
+    printf("\n");
+
+    /*
+    System name: Linux
+    Host name: newCS
+    OS Release: 3.13.0-36-generic
+    OS Version: #63-Ubuntu SMP Wed Sep 3 21:30:07 UTC 2014
+    CPU Type: x86_64
+    */
 
     return 0;
 }
