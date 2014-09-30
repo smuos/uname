8daadba5a9e8dcaa53b6a479bba9bc4b20f9ef0a
Tsubasa Hirooka <hirooka@bluenose.cs.dal.ca>
2014-09-30 17:04:11 -0300

    modified code

--- uname.c
+++ uname.c
@@ -2,20 +2,34 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <string.h>
+#include <string.h>
+#include <sys/utsname.h>
 
 int main(int argc, char *argv[]) {
 
-        struct UNAME_STRUCT ubuffer;
+    //struct UNAME_STRUCT ubuffer;
+	struct utsname ubuffer;
 
     // Check for proper usage
     if (argc != 1) {
         fprintf(stderr, "%s: Aborting, not enough arguments.\n", argv[0]);
         return (-1);
     }
+	
 
-    uname(&ubuffer);
-
-    printf("System name: %s\n", ubuffer.sysname);
-
+	//
+	/*if ((&ubuffer = malloc(sizeof(UNAME_STRUCT))) == NULL) {
+        fprintf(stderr, "%s: Could not allocate memory.\n", argv[0]);
+    }
+	*/
+    if( uname(&ubuffer) ==0 )
+	{
+		printf("System name: %s\n", ubuffer.sysname);
+		printf("Host name: %s\n", ubuffer.nodename);
+		printf("OS Release: %s\n", ubuffer.release);
+		printf("OS Version: %s\n", ubuffer.version);
+		printf("CPU Type: %s\n", ubuffer.machine);
+	}
+	
     return 0;
 }
