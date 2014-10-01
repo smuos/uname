57ca2d18b88c3a65a61618efb75fd409f1ab32ee
Joel Kuntz <joel.kuntz.92@gmail.com>
2014-10-01 11:14:13 -0300

    Fixed error code to conform to C standard
    see http://en.wikipedia.org/wiki/Exit_status#C_language

--- uname.c
+++ uname.c
@@ -11,7 +11,7 @@ int main(int argc, char *argv[]) {
     // Check for proper usage
     if (argc != 1) {
         fprintf(stderr, "%s: Aborting, too many arguments.\n", argv[0]);
-        return (-1);
+        return 1;
     }
 
     int rc = uname(&ubuffer);
