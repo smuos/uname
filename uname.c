6ea102505fb699aa38f3de1681a92da6b6b6fca2
Benjamin Sabean <ben_sabean@hotmail.com>
2014-10-02 20:06:14 +0000

    Program passes testing script.

--- uname.c
+++ uname.c
@@ -1,11 +1,15 @@
+//Written by Benjamin Sabean
+// A00337072
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
 #include <string.h>
+#include <sys/utsname.h>
 
 int main(int argc, char *argv[]) {
 
-        struct UNAME_STRUCT ubuffer;
+        struct utsname ubuffer;
 
     // Check for proper usage
     if (argc != 1) {
@@ -16,6 +20,10 @@ int main(int argc, char *argv[]) {
     uname(&ubuffer);
 
     printf("System name: %s\n", ubuffer.sysname);
+    printf("Host name: %s\n", ubuffer.nodename);
+    printf("OS Release: %s\n", ubuffer.release);
+    printf("OS Version: %s\n", ubuffer.version);
+    printf("CPU Type: %s\n", ubuffer.machine);
 
     return 0;
 }
