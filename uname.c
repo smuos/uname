3a07458b5ca52fad4a5904e081250427b4cba00e
mpsmith414 <mpsmith414@gmail.com>
2014-10-02 17:03:01 -0300

    added code to print out system information

--- uname.c
+++ uname.c
@@ -10,13 +10,19 @@ int main(int argc, char *argv[]) {
 
     // Check for proper usage
     if (argc != 1) {
-        fprintf(stderr, "%s: Aborting, not enough arguments.\n", argv[0]);
+        fprintf(stderr, "%s: Aborting, incorrect number of arguments.\n", argv[0]);
         return (-1);
     }
 
     uname(&ubuffer);
-
+    
+    // print out system information
     printf("System name: %s\n", ubuffer.sysname);
+    printf("Host name: %s\n", ubuffer.nodename);
+    printf("OS Release: %s\n", ubuffer.release);
+    printf("OS Version: %s\n", ubuffer.version);
+    printf("CPU Type: %s\n", ubuffer.machine);
+    
 
     return 0;
 }
