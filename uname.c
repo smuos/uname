d20fa61bde5b9c1cd7e7a2ec97edf72a64db1f5d
TommyymmoT <fawcetttom@hotmail.com>
2014-10-02 23:33:48 +0000

    cleaned up code and tested successfully

--- uname.c
+++ uname.c
@@ -13,16 +13,14 @@ int main(int argc, char *argv[]) {
     if (argc != 1) {
         fprintf(stderr, "%s: Aborting, not enough arguments.\n", argv[0]);
         return (-1);
-    
+    }
 
                     uname(&ubuffer);
-
                     printf("System name: %s\n", ubuffer.sysname);
                     printf("Host name: %s\n", ubuffer.nodename);
                     printf("OS Release: %s\n", ubuffer.release);
                     printf("OS Version: %s\n", ubuffer.version);
                     printf("CPU Type: %s\n", ubuffer.machine);
-
     	            return 0;
-		}		
+				
 }
