71ee5ce6ba653e2e8945e792fb246f26f0b96f0e
TommyymmoT <fawcetttom@hotmail.com>
2014-10-02 23:25:53 +0000

    Added more output messages and fixed stuct

--- uname.c
+++ uname.c
@@ -6,18 +6,23 @@
 
 
 int main(int argc, char *argv[]) {
-
-        struct UNAME_STRUCT ubuffer;
+	//correct the struct
+        struct utsname ubuffer;
 
     // Check for proper usage
     if (argc != 1) {
         fprintf(stderr, "%s: Aborting, not enough arguments.\n", argv[0]);
         return (-1);
-    }
+    
 
-    uname(&ubuffer);
+                    uname(&ubuffer);
 
-    printf("System name: %s\n", ubuffer.sysname);
+                    printf("System name: %s\n", ubuffer.sysname);
+                    printf("Host name: %s\n", ubuffer.nodename);
+                    printf("OS Release: %s\n", ubuffer.release);
+                    printf("OS Version: %s\n", ubuffer.version);
+                    printf("CPU Type: %s\n", ubuffer.machine);
 
-    return 0;
+    	            return 0;
+		}		
 }
