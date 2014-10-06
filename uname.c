e2ef29d7e68a810452f26ee4d209074027cb471b
Justin George <george.justin16@me.com>
2014-10-05 23:01:33 -0300

    All is good

--- uname.c
+++ uname.c
@@ -16,11 +16,11 @@ int main(int argc, char *argv[]) {
 
     uname(&ubuffer);
     
-    printf("System name: %s\n", uname -s);
-    printf("Host name: %s\n", uname -n);
-    printf("OS Release: %s\n", uname -r);
-    printf("OS Version: %s\n", uname -v);
-    printf("CPU Type: %s\n", uname -m);
+    printf("System name: %s\n", ubuffer.sysname);
+    printf("Host name: %s\n", ubuffer.nodename);
+    printf("OS Release: %s\n", ubuffer.release);
+    printf("OS Version: %s\n", ubuffer.version);
+    printf("CPU Type: %s\n", ubuffer.machine);
 
     return 0;
 }
