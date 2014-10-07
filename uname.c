19859a7a0b7795c6d0ac4f8e4309389c4dedfc91
JieZhang0918 <jz314907@dal.ca>
2014-10-06 22:45:18 -0300

    assighnment 2

--- uname.c
+++ uname.c
@@ -16,6 +16,11 @@ int main(int argc, char *argv[]) {
     uname(&ubuffer);
 
     printf("System name: %s\n", ubuffer.sysname);
-
-    return 0;
+   
+    printf("Host name: %s\n",ubuffer.nodename);
+    printf("OS Release: %s\n",ubuffer.release);
+    printf("OS Version: %s\n",ubuffer.version);
+    printf("CPU Type: %s\n",ubuffer.machine);
+   
+ return 0;
 }
