a8bacc32b43013728012cd6c9ee52b4c0a1fde70
yiyingZ <yiying@bluenose.cs.dal.ca>
2014-10-06 21:10:14 -0300

    Output more information

--- uname.c
+++ uname.c
@@ -19,6 +19,10 @@ int main(int argc, char *argv[]) {
     uname(&ubuffer);
 
     printf("System name: %s\n", ubuffer.sysname);
+    printf("Host name: %s\n", ubuffer.nodename);
+    printf("OS Release: %s\n", ubuffer.release);
+    printf("OS Version: %s\n", ubuffer.version);
+    printf("CPU Type: %s\n", ubuffer.machine);
 
     return 0;
 }
